eba88cbdcd90579303d9f14144567e04fa99abc3
Zephilinox <Zephilinox@hotmail.co.uk>
2025-09-04 17:11:15 +0100

    Update cmake and surpressions for clang-tidy and cppcheck

--- cmake/CPPCheck.cmake
+++ cmake/CPPCheck.cmake
@@ -1,21 +1,21 @@
 function(cppcheck_sources target_name sources)
     find_program(CPPCHECK cppcheck)
     if (NOT CPPCHECK)
-        message(ERROR "Failed to find cppcheck")
+        message(SEND_ERROR "Failed to find cppcheck - is it in your PATH?")
     endif()
     
     find_package(Python3)
     if (NOT Python3_FOUND)
-        message(ERROR "Failed to find 'python3'. It is required to use cppcheck")
+        message(SEND_ERROR "Failed to find 'python3'. It is required to use cppcheck")
     endif()
 
     if (NOT CMAKE_EXPORT_COMPILE_COMMANDS)
-        message(ERROR "'CMAKE_EXPORT_COMPILE_COMMANDS' is not set. It is required to use cppcheck")
+        message(SEND_ERROR "'CMAKE_EXPORT_COMPILE_COMMANDS' is not set. It is required to use cppcheck")
     endif()
 
     set(FILTER_COMPILE_COMMANDS_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/scripts/filter-compile-commands.py")
     if (NOT EXISTS "${FILTER_COMPILE_COMMANDS_SCRIPT}")
-        message(ERROR "'${FILTER_COMPILE_COMMANDS_SCRIPT}' does not exist. It is required to use cppcheck")
+        message(SEND_ERROR "'${FILTER_COMPILE_COMMANDS_SCRIPT}' does not exist. It is required to use cppcheck")
     endif()
 
     set(CPPCHECK_DIR "${CMAKE_CURRENT_BINARY_DIR}/cppcheck")
@@ -24,7 +24,13 @@ function(cppcheck_sources target_name sources)
         "--cppcheck-build-dir=${CPPCHECK_DIR}"
         "--error-exitcode=2"
         "--inline-suppr"
+        "--suppress=unusedFunction"
+        "--suppress=missingIncludeSystem"
+        "--suppress=useStlAlgorithm"
+        "--suppress=unmatchedSuppression"
+        "--check-level=exhaustive"
         "--project=compile_commands_filtered.json"
+        "--std=c++23"
     )
 
     add_custom_target("${target_name}"
@@ -43,7 +49,7 @@ function(cppcheck_sources target_name sources)
         COMMAND
             "${CMAKE_COMMAND}" "-E" "make_directory" "${CPPCHECK_DIR}"
         COMMAND
-            "${CPPCHECK}" "${CPPCHECK_COMMANDS}" "${sources}"
+            "${CPPCHECK}" "${CPPCHECK_COMMANDS}"
         COMMENT
             "Running cppcheck"
         COMMAND_EXPAND_LISTS
--- cmake/ClangTidy.cmake
+++ cmake/ClangTidy.cmake
@@ -1,26 +1,26 @@
 function(create_clang_tidy_check target_name)
     find_program(ClangTidy clang-tidy)
     if (NOT ClangTidy)
-        message(ERROR "Failed to find clang-tidy")
+        message(SEND_ERROR "Failed to find clang-tidy")
     endif()
     
     find_package(Python3)
     if (NOT Python3_FOUND)
-        message(ERROR "Failed to find 'python3'. It is required to use clang-tidy")
+        message(SEND_ERROR "Failed to find 'python3'. It is required to use clang-tidy")
     endif()
 
     if (NOT CMAKE_EXPORT_COMPILE_COMMANDS)
-        message(ERROR "'CMAKE_EXPORT_COMPILE_COMMANDS' is not set. It is required to use clang-tidy")
+        message(SEND_ERROR "'CMAKE_EXPORT_COMPILE_COMMANDS' is not set. It is required to use clang-tidy")
     endif()
 
     set(FILTER_COMPILE_COMMANDS_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/scripts/filter-compile-commands.py")
     if (NOT EXISTS "${FILTER_COMPILE_COMMANDS_SCRIPT}")
-        message(ERROR "'${FILTER_COMPILE_COMMANDS_SCRIPT}' does not exist. It is required to use clang-tidy")
+        message(SEND_ERROR "'${FILTER_COMPILE_COMMANDS_SCRIPT}' does not exist. It is required to use clang-tidy")
     endif()
 
     set(RUN_CLANG_TIDY_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/scripts/run-clang-tidy.py")
     if (NOT EXISTS "${RUN_CLANG_TIDY_SCRIPT}")
-        message(ERROR "'${RUN_CLANG_TIDY_SCRIPT}' does not exist. It is required to use clang-tidy")
+        message(SEND_ERROR "'${RUN_CLANG_TIDY_SCRIPT}' does not exist. It is required to use clang-tidy")
     endif()
 
     set(CLANG_TIDY_COMMANDS
